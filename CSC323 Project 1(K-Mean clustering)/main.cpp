de86e847da918b97c389fc454a736e60ef74a855
Joel Morel <JaM@joels-macbook-air.local>
2018-02-11 17:12:21 -0500

    Initial Commit

---
+++ CSC323 Project 1(K-Mean clustering)/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CSC323 Project 1(K-Mean clustering)
+//
+//  Created by Joel Morel on 2/11/18.
+//  Copyright © 2018 Joel Morel. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
